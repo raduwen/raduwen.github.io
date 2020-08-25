6228342a8afbc248eaefcd764329fd7cdf87a162
ラドウェン <raduwen@gmail.com>
2020-08-26 02:04:20 +0900

    fix link target

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ function HomePage() {
       <a href="https://bbs.jpnkn.com/raduwen/" target="_blank">掲示板</a><br/>
       <a href="https://stamp.archsted.com/291" target="_blank">スタンプキャスト</a><br/>
       <a href="https://www.twitch.tv/raduwen" target="_blank">Twitch</a><br/>
-      <a href="https://www.youtube.com/channel/UC9qkQ8wBNtykTaLrNHh_IJQ/">YouTube</a>(特に何もないかも)<br/>
+      <a href="https://www.youtube.com/channel/UC9qkQ8wBNtykTaLrNHh_IJQ/" target="_blank">YouTube</a>(特に何もないかも)<br/>
     </div>
   )
 }
