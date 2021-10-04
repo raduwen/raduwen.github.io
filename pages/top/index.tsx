8ad8475d556c984c0ad4593bd33c466f70fc0e3a
raduwen <raduwen@gmail.com>
2021-10-05 08:27:41 +0900

    add news

--- pages/top/index.tsx
+++ pages/top/index.tsx
@@ -66,6 +66,8 @@ const HomePage = () => {
             <section>
               <h2>NEWS</h2>
               <dl>
+                <dt>2021/10/05</dt>
+                <dd>フォントを修正</dd>
                 <dt>2021/01/19</dt>
                 <dd>メニューアイコン追加</dd>
                 <dt>2021/01/11</dt>
