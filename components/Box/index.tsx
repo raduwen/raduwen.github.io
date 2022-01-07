476b77525465908d17cc20ece715ddcdae54f427
raduwen <raduwen@gmail.com>
2022-01-08 05:23:08 +0900

    emotion Box

--- components/Box/index.tsx
+++ components/Box/index.tsx
@@ -1,41 +1,29 @@
-import { FC } from 'react'
-const borderStyle = '2px solid black'
+import styled from '@emotion/styled';
 
-const style = {
-  body: {
-    backgroundColor: 'rgb(255, 221, 187)',
-    padding: '16px 32px',
-    borderBottom: borderStyle,
-  },
-  bar: {
-    borderBottom: borderStyle,
-    backgroundColor: 'rgb(85, 119, 35)',
+const border = '2px solid black';
+
+const BoxBody = styled.div`
+  background-color: rgb(255, 221, 187);
+  padding: 1.6rem 3.2rem;
+`;
+
+const BoxBar = styled.div`
+  background-color: rgb(85, 119, 35);
+
+  &:first-child {
+    border-bottom: ${border}
   }
-}
 
-const Box: FC = ({ children }) => {
-  return (
-    <div style={{ borderTop: borderStyle, borderRight: borderStyle, borderLeft: borderStyle, width: '75%', minWidth: '396px' }}>
-      {children}
-    </div>
-  )
-}
+  &:last-child {
+    border-top: ${border}
+  }
+`;
 
-const BoxBody: FC = ({ children }) => {
-  return (
-    <div style={style.body}>
-      {children}
-    </div>
-  )
-}
+const Box = styled.div`
+  border: ${border};
+  width: 75%;
+  min-width: 396px;
+`;
 
-const BoxBar: FC = ({ children }) => {
-  return (
-    <div style={style.bar}>
-      {children}
-    </div>
-  )
-}
-
-export { BoxBody, BoxBar }
-export default Box
+export { BoxBody, BoxBar };
+export default Box;
