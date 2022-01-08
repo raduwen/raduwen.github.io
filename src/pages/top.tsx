e6ee05c98c6ef3bab7e23fbc76638fabd49a998b
raduwen <raduwen@gmail.com>
2022-01-08 16:40:15 +0900

    add logo and under development text

--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -1,7 +1,22 @@
+import Image from 'next/image'
+import { Box, Heading, Text } from '@chakra-ui/react'
+
 import { MainLayout } from '@/layouts/MainLayout'
 
 const TopPage = () => {
-  return <div>top</div>
+  return (
+    <Box border="2px" bg="orange.50" p="8" minW="480px" w="80%" minH="320px">
+      <Box display="flex" justifyContent="center">
+        <Image src="/images/logo.png" width="332" height="32" />
+      </Box>
+
+      <Box mt="16">
+        <Text fontSize="xl" textAlign="center">
+          工事中だよ
+        </Text>
+      </Box>
+    </Box>
+  )
 }
 
 TopPage.getLayout = (page: JSX.Element) => {
