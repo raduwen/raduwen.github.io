812167c73edb7f7feeba17dd654562fa7ece2957
raduwen <raduwen@gmail.com>
2022-01-08 20:11:37 +0900

    add link to top at root

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
+import { Heading, Text, Box, Container } from '@chakra-ui/react'
 
 const IndexPage: NextPage = () => {
-  return <div>hello</div>
+  return (
+    <Container size="container.lg">
+      <Box my={16} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+        <Heading my={8}>ラドウェンのラボ</Heading>
+        <Text fontSize="xl">
+          <Link href="/top">
+            - ENTER -
+          </Link>
+        </Text>
+      </Box>
+    </Container>
+  )
 }
 
 export default IndexPage
