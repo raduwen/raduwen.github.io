303451b2d621358a9166e70b208106e7d2898d16
raduwen <raduwen@gmail.com>
2022-01-08 18:38:07 +0900

    split main menu info

--- src/components/MainMenu/index.tsx
+++ src/components/MainMenu/index.tsx
@@ -2,28 +2,17 @@ import { Box } from '@chakra-ui/react'
 
 import { MenuList } from './MenuList'
 import { MenuListItem } from './MenuListItem'
+import { mainMenus } from './mainMenus'
 
 type MainMenuProps = {
   onSelected: (value: string) => void
 }
 
 const MainMenu = ({ onSelected }: MainMenuProps) => {
-  const menus = [
-    { color: 'a', text: 'トップ', sub: 'top', image: '/images/top.png' },
-    { color: 'b', text: 'ホゲ', sub: 'hoge', image: '/images/hoge.png' },
-    { color: 'c', text: 'ペカ', sub: 'peca', image: '/images/stachu.png' },
-    { color: 'd', text: 'ゲーム', sub: 'game', image: '/images/construction.png' },
-    { color: 'e', text: 'プログ', sub: 'prog', image: '/images/construction.png' },
-    { color: 'f', text: '音楽', sub: 'music', image: '/images/construction.png' },
-    { color: 'g', text: '????', sub: 'yobi1', image: '/images/construction.png' },
-    { color: 'h', text: '????', sub: 'yobi2', image: '/images/construction.png' },
-    { color: 'i', text: '????', sub: 'yobi3', image: '/images/construction.png' },
-  ]
-
   return (
     <Box>
       <MenuList>
-        {menus.map((menu) => (
+        {mainMenus.map((menu) => (
           <MenuListItem
             key={menu.sub}
             color={menu.color}
