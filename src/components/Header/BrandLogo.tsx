2d2618ace8f39fe0b254bc3626d00ccc45b9faaf
raduwen <raduwen@gmail.com>
2022-01-08 16:51:29 +0900

    add brand logo (temporary)

--- src/components/Header/BrandLogo.tsx
+++ src/components/Header/BrandLogo.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import { Box } from '@chakra-ui/react'
 
 const BrandLogo = () => {
   return (
-    <Box w="134px" h="44px" border="2px solid black">
-      TODO Image
+    <Box>
+      <Image src="/images/brand_logo.png" width="134px" height="44px" />
     </Box>
   )
 }
